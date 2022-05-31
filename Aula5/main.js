34928b3c96d45dc574798c31dbee49875cd72d1d
Lucas_Ramalho <lucasramalhon@yahoo.com.br>
2022-05-30 22:48:35 -0300

    mesa-05-31-05-22

--- Aula5/main.js
+++ Aula5/main.js
@@ -1,13 +1,75 @@
-console.log('O script carregou')
 
-const bodyElement = document.querySelector('body')
+var containerElemento = document.querySelector('.container')
+
+var posts = [
+    {
+        foto: 'https://s3.static.brasilescola.uol.com.br/be/2022/01/tigre.jpg',
+        titulo: 'O tigre',
+        texto: 'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
+    },
+    {
+        foto: 'https://vejasp.abril.com.br/wp-content/uploads/2021/12/20211202-Tigre-de-bengala9-foto-Paulo-Gil-2.jpg-1.jpg',
+        titulo: 'O tigre versão Shine',
+        texto: 'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
+    },
+    {
+        foto: './imagens/chita.jpg',
+        titulo: 'A chita',
+        texto: 'A chita (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+    {
+        foto: './imagens/jaguar.jpg',
+        titulo: 'O Jaguar',
+        texto: 'O Jaguar (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+    {
+        foto: './imagens/leon.jpg',
+        titulo: 'O leão',
+        texto: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+    {
+        foto: './imagens/leopardo.jpg',
+        titulo: 'O Leopardo',
+        texto: 'O Leopardo (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+    {
+        foto: './imagens/pantera-negra.jpg',
+        titulo: 'A Pantera Negra',
+        texto: 'A Pantera Negra (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+    {
+        foto: './imagens/tiger.jpg',
+        titulo: 'O Tigre',
+        texto: 'O Tigre (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+    },
+  
+]
 
 
-function trocarCor(){
+for(let post of posts) {
 
-    //bodyElement.classList.add('backgroung-red')
-    bodyElement.classList.toggle('backgroung-red')
-    console.log(bodyElement)
-    console.log('A cor foi trocada')
+    containerElemento.innerHTML += `
+        <div class="item">
+            <img src="${post.foto}">
+            <h2>${post.titulo}</h2>
+            <p>${post.texto}</p>
+        </div>
+    `
 
 }
+
+function adicionarPost(){
+    var newPost = {
+        foto: prompt('Insira um foto'),
+        titulo: prompt('Insira um titulo'),
+        texto: prompt('insira um texto')
+    }
+    console.log(newPost)
+}
+
+
+
+
+
+
+
